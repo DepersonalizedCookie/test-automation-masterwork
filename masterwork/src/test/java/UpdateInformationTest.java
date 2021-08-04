5bf62f5decd3542c5c285d3e3aaa3a99a9d36819
DepersonalizedCookie <batorfidada@gmail.com>
2021-08-04 18:38:14 +0200

    Add UpdateInformationTest.java

---
+++ masterwork/src/test/java/UpdateInformationTest.java
@@ -0,0 +1,45 @@
+import io.qameta.allure.*;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.support.PageFactory;
+import pages.*;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Epic("User account management")
+public class UpdateInformationTest extends BaseTest {
+  HomePage homePage;
+  LoginPage loginPage;
+
+  @BeforeEach
+  public void setUpPreconditions() {
+    homePage = PageFactory.initElements(driver, HomePage.class);
+    homePage.open();
+    homePage.navigateToLoginPage();
+    loginPage = PageFactory.initElements(driver, LoginPage.class);
+    assertThat(loginPage.isLoaded()).isTrue();
+  }
+
+  @Step
+  public void makeSureLoginIsSuccessful() {
+    loginPage.login("herman@gmail.com", "KidnapTheSandyClaws");
+    assertThat(loginPage.isSuccessful()).isTrue();
+  }
+
+  @Test
+  @DisplayName("TC10_Update Data")
+  @Feature("Changing user account information.")
+  @Story("After changing user account information, the success message should be correct.")
+  void updateInformation() {
+    makeSureLoginIsSuccessful();
+    MyAccountPage myAccountPage = PageFactory.initElements(driver, MyAccountPage.class);
+    myAccountPage.navigateToEditAccount();
+    MyAccountInformationPage myAccountInformationPage = PageFactory
+            .initElements(driver, MyAccountInformationPage.class);
+    myAccountInformationPage.modifyLastName("Herrman");
+    assertThat(myAccountPage.getSuccessMessage())
+            .isEqualTo("Success: Your account has been successfully updated.");
+  }
+
+
+}
