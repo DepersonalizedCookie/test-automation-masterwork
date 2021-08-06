52cdf3047cf2f3d1438d9eb65cef195e7c720dfc
DepersonalizedCookie <batorfidada@gmail.com>
2021-08-06 08:37:20 +0200

    Modify BaseTest.java

--- masterwork/src/test/java/BaseTest.java
+++ masterwork/src/test/java/BaseTest.java
@@ -5,14 +5,12 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.Properties;
 
 
@@ -23,9 +21,6 @@ public class BaseTest {
 
   @BeforeEach
   public void setup() throws IOException {
-    HashMap<String, Object> options = new HashMap<>();
-    options.put("download.default_directory", "downloads");
-
     Properties properties = new Properties();
     InputStream resourceAsStream = this.getClass().getResourceAsStream("/test.properties");
     properties.load(resourceAsStream);
@@ -33,15 +28,15 @@ public class BaseTest {
     if (browser.equals("chrome")) {
       WebDriverManager.chromedriver().setup();
       this.driver = new ChromeDriver();
-      ChromeOptions chromeOptions = new ChromeOptions();
-      chromeOptions.setExperimentalOption("prefs", options);
       wait = new WebDriverWait(driver,5);
     } else if (browser.equals("firefox")) {
       WebDriverManager.firefoxdriver().setup();
       this.driver = new FirefoxDriver();
+      wait = new WebDriverWait(driver,5);
     } else {
       WebDriverManager.edgedriver().setup();
       this.driver = new EdgeDriver();
+      wait = new WebDriverWait(driver,5);
     }
 
   }
