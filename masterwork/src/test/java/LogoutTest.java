44f8380cc5661c8c91b46267c5cfcb18ff0b57cd
DepersonalizedCookie <batorfidada@gmail.com>
2021-08-17 17:57:35 +0200

    Modify LogoutTest.java

--- masterwork/src/test/java/LogoutTest.java
+++ masterwork/src/test/java/LogoutTest.java
@@ -10,8 +10,6 @@ public class LogoutTest extends BaseTest{
   HomePage homePage;
   LoginPage loginPage;
 
-
-
   @BeforeEach
   public void setUpPreconditions() {
     homePage = PageFactory.initElements(driver, HomePage.class);
@@ -21,15 +19,11 @@ public class LogoutTest extends BaseTest{
     assertTrue(loginPage.isLoaded());
   }
 
-  public void makeSureLoginIsSuccessful() {
-    loginPage.login("herman@gmail.com", "KidnapTheSandyClaws");
-    makeScreenshot();
-    assertThat(loginPage.isSuccessful()).isTrue();
-  }
   @Test
   @DisplayName("TC13_Logout")
   @Feature("Logout")
-  @Story("After successful login, and arriving on the My account page, log out and assert that the title is correct.")
+  @Story("After a successful login attempt and arrival on the \"My Account Page\", if the user logs out the title should be correct ")
+  @Description("Makes a valid login attempt, and after arriving on the \"My Account Page\", clicks on the \"Log out\" button, and asserts that the title is correct.")
   public void logout() {
     makeSureLoginIsSuccessful();
     MyAccountPage myAccountPage = PageFactory.initElements(driver, MyAccountPage.class);
