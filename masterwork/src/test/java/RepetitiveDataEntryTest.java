9ed4781bfafb7f47a171dc1d89a34a13588505f2
DepersonalizedCookie <batorfidada@gmail.com>
2021-08-18 10:59:10 +0200

    Modify RepetitiveDataEntryTest.java

--- masterwork/src/test/java/RepetitiveDataEntryTest.java
+++ masterwork/src/test/java/RepetitiveDataEntryTest.java
@@ -1,5 +1,5 @@
 import io.qameta.allure.*;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,24 +8,11 @@ import org.openqa.selenium.support.PageFactory;
 import pages.*;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @Tag("new-address")
 @DisplayName("TC9_Create new address")
 @Epic("Address book management")
 public class RepetitiveDataEntryTest extends BaseTest {
-  HomePage homePage;
-  LoginPage loginPage;
-
-
-  @BeforeEach
-  public void setUpPreconditions() {
-    homePage = PageFactory.initElements(driver, HomePage.class);
-    homePage.open();
-    homePage.navigateToLoginPage();
-    loginPage = PageFactory.initElements(driver, LoginPage.class);
-    assertTrue(loginPage.isLoaded());
-  }
 
   @ParameterizedTest(name = "TC9_Create new address - Fistname = {0}, Lastname = {1}, Address1 = {2}, City = {3}, Postcode = {4}, Country = {5}, State = {6}")
   @Tag("addresses")
@@ -34,6 +21,7 @@ public class RepetitiveDataEntryTest extends BaseTest {
   @Description("Adds new addresses from a CSV file source, and asserts that the success message is correct.")
   @CsvFileSource(resources = "addresses.csv")
   void shouldCreateNewAddress(String firstname, String lastname, String address1, String city, String postcode, String country, String state) {
+    navigateToLoginPage();
     makeSureLoginIsSuccessful();
     navigateToAddressBookPage();
     AddressBookPage addressBookPage = PageFactory.initElements(driver, AddressBookPage.class);
