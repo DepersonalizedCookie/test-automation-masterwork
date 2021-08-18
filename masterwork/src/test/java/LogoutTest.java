341dbdc93ad5fe9ab390e9d3269542714266b680
DepersonalizedCookie <batorfidada@gmail.com>
2021-08-18 10:32:24 +0200

    Modify LogoutTest.java

--- masterwork/src/test/java/LogoutTest.java
+++ masterwork/src/test/java/LogoutTest.java
@@ -4,20 +4,9 @@ import org.openqa.selenium.support.PageFactory;
 import pages.*;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
+@Epic("User account management")
 public class LogoutTest extends BaseTest{
-  HomePage homePage;
-  LoginPage loginPage;
-
-  @BeforeEach
-  public void setUpPreconditions() {
-    homePage = PageFactory.initElements(driver, HomePage.class);
-    homePage.open();
-    homePage.navigateToLoginPage();
-    loginPage = PageFactory.initElements(driver, LoginPage.class);
-    assertTrue(loginPage.isLoaded());
-  }
 
   @Test
   @DisplayName("TC13_Logout")
@@ -25,6 +14,7 @@ public class LogoutTest extends BaseTest{
   @Story("After a successful login attempt and arrival on the \"My Account Page\", if the user logs out the title should be correct ")
   @Description("Makes a valid login attempt, and after arriving on the \"My Account Page\", clicks on the \"Log out\" button, and asserts that the title is correct.")
   public void logout() {
+    navigateToLoginPage();
     makeSureLoginIsSuccessful();
     MyAccountPage myAccountPage = PageFactory.initElements(driver, MyAccountPage.class);
     myAccountPage.logout();
