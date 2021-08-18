a1891797679a97b183bc9f85e72515d37fc18706
DepersonalizedCookie <batorfidada@gmail.com>
2021-08-18 16:29:42 +0200

    Modify HomePage.java

--- masterwork/src/test/java/pages/HomePage.java
+++ masterwork/src/test/java/pages/HomePage.java
@@ -1,10 +1,13 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,13 +20,7 @@ public class HomePage extends BasePage {
   }
 
   @FindBy(xpath = "//*[@id=\"top-links\"]/ul/li[2]/a")
-  private WebElement myAccountDropDown;
-
-  @FindBy(linkText = "Login")
-  private WebElement login;
-
-  @FindBy(linkText = "Register")
-  private WebElement register;
+  WebElement myAccountDropDown;
 
   @FindBy(name = "search")
   WebElement searchInput;
@@ -37,6 +34,8 @@ public class HomePage extends BasePage {
   @FindBy(linkText = "Laptops & Notebooks")
   WebElement laptopsNotebooksDropDown;
 
+  @FindBy(id = "wishlist-total")
+  WebElement wishListLink;
 
   public void open() {
     driver.get(url);
@@ -48,29 +47,16 @@ public class HomePage extends BasePage {
     return driver.getTitle().equals("Your Store");
   }
 
-
-  private WebElement getRegister() {
-    return register;
-  }
-
-  private WebElement getLogin() {
-    return login;
-  }
-
-  private WebElement getMyAccountDropDown() {
-    return myAccountDropDown;
-  }
-
   public void navigateToRegistrationPage() {
-    getMyAccountDropDown().click();
-    wait.until(ExpectedConditions.elementToBeClickable(getRegister()));
-    getRegister().click();
+    myAccountDropDown.click();
+    WebElement register = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.linkText("Register"))));
+    register.click();
   }
 
   public void navigateToLoginPage() {
-    getMyAccountDropDown().click();
-    wait.until(ExpectedConditions.elementToBeClickable(getLogin()));
-    getLogin().click();
+    myAccountDropDown.click();
+    WebElement login = wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.linkText("Login"))));
+    login.click();
   }
 
   public void search(String query) {
@@ -83,11 +69,25 @@ public class HomePage extends BasePage {
     assertThat(driver.getTitle()).isEqualTo("Find Your Favorite Brand");
   }
 
-  public void getAllLaptopsNoteBooks() {
-    laptopsNotebooksDropDown.click();
-    WebElement showAll = wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Show All Laptops & Notebooks")));
-    showAll.click();
+  public void addFeaturedToWishList() {
+    List<WebElement> wishListButtons = driver.findElements(By.xpath("//button[@data-original-title ='Add to Wish List']"));
+    for (int i = 0; i < wishListButtons.size(); i++) {
+      if (wishListButtons.get(i).isEnabled()) {
+        wait.until(ExpectedConditions.elementToBeClickable(wishListButtons.get(i)));
+        wishListButtons.get(i).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert-success")));
+        WebElement successMessage = driver.findElement(By.className("alert-success"));
+        if (successMessage.isDisplayed() && successMessage.getText()
+                .contains(" to your wish list")) {
+          JavascriptExecutor jse = (JavascriptExecutor) driver;
+          jse.executeScript("scroll(0, 250)");
+        }
+      }
+    }
   }
 
 
+  public void navigateToWishList() {
+    wishListLink.click();
+  }
 }
