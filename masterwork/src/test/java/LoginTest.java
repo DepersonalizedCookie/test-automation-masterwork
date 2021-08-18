80ae24e8807550ce85f076b6cf3ae913d7944b68
DepersonalizedCookie <batorfidada@gmail.com>
2021-08-18 10:31:38 +0200

    Modify LoginTest.java

--- masterwork/src/test/java/LoginTest.java
+++ masterwork/src/test/java/LoginTest.java
@@ -4,21 +4,15 @@ import org.openqa.selenium.support.PageFactory;
 import pages.*;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 @Epic("User account management")
 public class LoginTest extends BaseTest {
-  HomePage homePage;
   LoginPage loginPage;
 
-  @BeforeEach
+  @Step("Initializes the \"Login Page\".")
   public void setUpPage() {
-    homePage = PageFactory.initElements(driver, HomePage.class);
-    homePage.open();
-    homePage.navigateToLoginPage();
     loginPage = PageFactory.initElements(driver, LoginPage.class);
-    assertTrue(loginPage.isLoaded());
   }
 
   @Test
@@ -26,6 +20,8 @@ public class LoginTest extends BaseTest {
   @DisplayName("TC3_Valid Login")
   @Story("After a login attempt with valid credentials, assert that the title is the My Account Page title.")
   public void validLogin() {
+    navigateToLoginPage();
+    setUpPage();
     loginPage.login("herman@gmail.com", "KidnapTheSandyClaws");
     makeScreenshot();
     assertThat(loginPage.isSuccessful()).isTrue();
@@ -36,6 +32,8 @@ public class LoginTest extends BaseTest {
   @DisplayName("TC4_Invalid Login")
   @Story("After a login attempt with invalid credentials, assert that the error message is correct.")
   public void invalidLogin() {
+    navigateToLoginPage();
+    setUpPage();
     loginPage.login("belongsToNoOne@gmail.com", "KidnapTheSandyClaws");
     makeScreenshot();
     assertThat(loginPage.isUnsuccessful()).isTrue();
