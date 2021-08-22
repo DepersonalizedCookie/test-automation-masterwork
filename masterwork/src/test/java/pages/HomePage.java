20d027ef92c0d415fa0129d6a8e0238f4d259654
DepersonalizedCookie <batorfidada@gmail.com>
2021-08-22 20:35:34 +0200

    Modify HomePage.java

--- masterwork/src/test/java/pages/HomePage.java
+++ masterwork/src/test/java/pages/HomePage.java
@@ -1,13 +1,11 @@
 package pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,9 +31,6 @@ public class HomePage extends BasePage {
 
   @FindBy(linkText = "Laptops & Notebooks")
   WebElement laptopsNotebooksDropDown;
-
-  @FindBy(id = "wishlist-total")
-  WebElement wishListLink;
 
   public void open() {
     driver.get(url);
@@ -68,26 +63,10 @@ public class HomePage extends BasePage {
     brands.click();
     assertThat(driver.getTitle()).isEqualTo("Find Your Favorite Brand");
   }
-
-  public void addFeaturedToWishList() {
-    List<WebElement> wishListButtons = driver.findElements(By.xpath("//button[@data-original-title ='Add to Wish List']"));
-    for (int i = 0; i < wishListButtons.size(); i++) {
-      if (wishListButtons.get(i).isEnabled()) {
-        wait.until(ExpectedConditions.elementToBeClickable(wishListButtons.get(i)));
-        wishListButtons.get(i).click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("alert-success")));
-        WebElement successMessage = driver.findElement(By.className("alert-success"));
-        if (successMessage.isDisplayed() && successMessage.getText()
-                .contains(" to your wish list")) {
-          JavascriptExecutor jse = (JavascriptExecutor) driver;
-          jse.executeScript("scroll(0, 250)");
-        }
-      }
-    }
+  public void getAllLaptopsNoteBooks() {
+    laptopsNotebooksDropDown.click();
+    WebElement showAll = wait.until(ExpectedConditions.presenceOfElementLocated(By.linkText("Show All Laptops & Notebooks")));
+    showAll.click();
   }
 
-
-  public void navigateToWishList() {
-    wishListLink.click();
-  }
 }
