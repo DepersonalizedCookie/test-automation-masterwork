2b0d819f747e0b3963b46882d7c49f92aed70857
DepersonalizedCookie <batorfidada@gmail.com>
2021-08-22 20:36:09 +0200

    Modify DownloadDataTest.java

--- masterwork/src/test/java/DownloadDataTest.java
+++ masterwork/src/test/java/DownloadDataTest.java
@@ -1,9 +1,11 @@
 import io.qameta.allure.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import org.openqa.selenium.support.PageFactory;
-import pages.*;
+import pages.HomePage;
+import pages.LaptopsNotebooksPage;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -16,62 +18,55 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-@Epic("List products by Category")
-public class DownloadDataTest extends BaseTest {
+  @Epic("List products by Category")
+  public class DownloadDataTest extends BaseTest {
+    HomePage homePage;
 
-  @Step("Adds elements to the wishlist")
-  public void addToWishlist() {
-    MyAccountPage myAccountPage = PageFactory.initElements(driver, MyAccountPage.class);
-    myAccountPage.navigateToHomePage();
-    HomePage homePage = PageFactory.initElements(driver, HomePage.class);
-    homePage.addFeaturedToWishList();
-  }
+    @BeforeEach
+    public void setUpPreconditions() {
+      homePage = PageFactory.initElements(driver, HomePage.class);
+      homePage.open();
+    }
 
+    @Step
+    public void getAllLapTopsAndNoteBooks() {
+      homePage.getAllLaptopsNoteBooks();
+      assertThat(driver.getTitle()).isEqualTo("Laptops & Notebooks");
+    }
 
-  @Step("Navigates to the wishlist from the \"Home Page\".")
-  public void goToWishList() {
-    HomePage homePage = PageFactory.initElements(driver, HomePage.class);
-    homePage.navigateToWishList();
-    assertThat(driver.getTitle()).isEqualTo("My Wish List");
-  }
+    @Step
+    public void writeFile() {
+      LaptopsNotebooksPage laptopsNotebooksPage = PageFactory.initElements(driver, LaptopsNotebooksPage.class);
+      List<String> fileContent = laptopsNotebooksPage.getProductNames();
+      Path filePath = Paths.get("src/test/resources/productNames.txt");
+      try {
+        Files.write(filePath, fileContent);
+      } catch (IOException e) {
+        System.err.println("Can not write file!");
+      }
+    }
 
-  @Step("Writes a file from the wishlist elements names")
-  public void writeFile() {
-    WishListPage wishListPage = PageFactory.initElements(driver, WishListPage.class);
-    List<String> fileContent = wishListPage.getWishListProductsPrices();
-    Path filePath = Paths.get("src/test/resources/wishListProductPrices.txt");
-    try {
-      Files.write(filePath, fileContent);
-    } catch (IOException e) {
-      System.err.println("Can not write file!");
+    @Step
+    public List<String> readFileContent() {
+      String folder = "src/test/resources/";
+      List<String> result = new ArrayList<>(Arrays.asList("", ""));
+      try {
+        result = new ArrayList<>(Files.readAllLines(Paths.get(folder + "productNames.txt")));
+      } catch (IOException e) {
+        System.err.println("Can not read file!");
+      }
+      return result;
+    }
+
+    @Test
+    @DisplayName("TC12_Data Download")
+    @Feature("Product listing")
+    @Story("When listing all products from a product category, the products should appear.")
+    @Description("When listing all products from a product category, and exporting the product names to file, the file content should match with the names.")
+    public void downloadProductsNames() {
+      getAllLapTopsAndNoteBooks();
+      writeFile();
+      LaptopsNotebooksPage laptopsNotebooksPage = PageFactory.initElements(driver, LaptopsNotebooksPage.class);
+      assertThat(readFileContent()).isEqualTo(laptopsNotebooksPage.getProductNames());
     }
   }
-
-
-  @Step("Reads the content of the file, and returns it as a string list")
-  public List<String> readFileContent() {
-    String folder = "src/test/resources/";
-    List<String> result = new ArrayList<>(Arrays.asList("", ""));
-    try {
-      result = new ArrayList<>(Files.readAllLines(Paths.get(folder + "wishListProductPrices.txt")));
-    } catch (IOException e) {
-      System.err.println("Can not read file!");
-    }
-    return result;
-  }
-
-  @Test
-  @DisplayName("TC12_Data Download")
-  @Feature("Product listing")
-  @Story("When listing all products from a product category, the products should appear.")
-  @Description("When listing all products from a product category, and exporting the product prices to file, the file content should match with the names.")
-  public void downloadProductsNames() {
-    navigateToLoginPage();
-    makeSureLoginIsSuccessful();
-    addToWishlist();
-    goToWishList();
-    writeFile();
-    WishListPage wishListPage = PageFactory.initElements(driver, WishListPage.class);
-    assertThat(readFileContent()).isEqualTo(wishListPage.getWishListProductsPrices());
-  }
-}
